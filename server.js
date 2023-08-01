65794eb6e8bf7c3825134f638e6f64aa10c94598
joshmijoseph <joshmijjoseph@gmail.com>
2023-08-01 08:49:19 -0400

    Setting up local port and compatibility w/ Heroku

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = 3000; // Choose any available port number
+
+const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
